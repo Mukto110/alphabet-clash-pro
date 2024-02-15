99603f7801b7b1b8149af55680b8f7c3a05eaf0b
Mukto110 <merajhossainmukto@gmail.com>
2024-02-15 17:36:17 +0600

    alphabet function added

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,21 @@
+// function play(){
+//     // step 1: hide the home screen. to hide the screen add the class hidden to the home section
+//      const homeSec = document.getElementById('home');
+//      homeSec.classList.add('hidden');
+//     // step 2: show the playground
+//     const playgroundSec = document.getElementById('play-ground');
+//     playgroundSec.classList.remove('hidden');
+// }
+
+function continueGame(){
+    // step - 1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log("Your random alphabet" ,alphabet);
+
+}
+
+function play(){
+    hideElementById('home');
+    showElementById('play-ground');
+    continueGame();
+}
