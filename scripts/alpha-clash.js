cbe96d9ad5805b85951e602cebcd4d765bd27ae1
Mukto110 <merajhossainmukto@gmail.com>
2024-02-17 04:02:25 +0600

    keyboard gackground color setup done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -12,6 +12,13 @@ function continueGame(){
     const alphabet = getARandomAlphabet();
     console.log("Your random alphabet" ,alphabet);
 
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+
 }
 
 function play(){
